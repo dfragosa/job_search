4056b32b7e560b66a9ae4a3565547b940eb27345
Derric <54610360+dfragosa@users.noreply.github.com>
2020-03-11 11:27:35 -0400

    update gemfile, add httparty & pry

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,14 @@
-class Api
-  ##
+class API
+  
+  def self.get_data(choice)
+   
+   response = HTTParty.get("https://jobs.github.com/positions.json?description=#{choice}&page=1")
+   binding.pry
+   response[0] #.each do |hash| 
+    Jobs.new.title
+   #end
+  binding.pry
+  end
+
   
 end
-
