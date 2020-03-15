c088ea1d44687bb0d8ec693be69377b1221ceda7
Derric <54610360+dfragosa@users.noreply.github.com>
2020-03-14 21:32:19 -0400

    renamed gem created job_search.rb to environment, delete executable file in the environment file

--- bin/executable.rb
+++ bin/executable.rb
@@ -3,8 +3,7 @@
  # executable / run file as per example in cohort video
 
 require "bundler/setup"
-require "job_search"
+require_relative '../lib/environment'
 
-#require_relative '../lib/job_search'
 
 CLI.new.start
